2abdd2332cdeeee9a4ed6271b58db3f2935935ad
Tu <tucq88@gmail.com>
2017-11-02 07:01:40 +0700

    Add modal box + validation

--- resources/assets/js/components/LandingNotify.vue
+++ resources/assets/js/components/LandingNotify.vue
@@ -2,19 +2,47 @@
   <div class="box box-notify">
     <div class="field is-grouped">
       <p class="control is-expanded">
-        <input class="input" type="text" placeholder="Enter your email">
+        <input class="input" type="email" placeholder="Enter your email" v-model="email">
       </p>
       <p class="control">
-        <a class="button is-info">Notify Me</a>
+        <button class="button is-info" :disabled="inValid(email)" v-on:click="subscribe(email)">Notify Me</button>
       </p>
+    </div>
+    <div class="modal" v-bind:class="{ 'is-active': openModal }">
+      <div class="modal-background"></div>
+      <div class="modal-content content">
+        <h3>Thanks for subscribing</h3>
+        <p>We're working very hard. We'll let you know when things are ready for you as soon as possible.</p>
+        <p><button class="button is-primary" v-on:click="triggerModal">Close</button></p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    mounted() {
-      console.log('Component mounted.')
+    data: () => {
+      return {
+        email: '',
+        openModal: false
+      }
+    },
+    methods: {
+      inValid: function(input) {
+        return input.trim() === '' || !this.isEmail(input);
+      },
+      isEmail: function(string) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          .test(string);
+      },
+      subscribe: function(email) {
+        console.log(email);
+        this.triggerModal();
+        this.email = '';
+      },
+      triggerModal: function() {
+        this.openModal = !this.openModal;
+      }
     }
   }
 </script>
